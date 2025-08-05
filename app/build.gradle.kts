fb7d1dd164c0653650563d549e5106388c068301
Muhammet Fatih Şahan <alkan2798@gmail.com>
2025-08-06 00:18:20 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,71 @@
+plugins {
+	alias(libs.plugins.android.application)
+	alias(libs.plugins.kotlin.android)
+	alias(libs.plugins.ksp)
+	alias(libs.plugins.navigation.safeargs.kotlin)
+}
+
+android {
+	namespace = "com.ncorp.hesapp"
+	compileSdk = 36
+
+	defaultConfig {
+		applicationId = "com.ncorp.hesapp"
+		minSdk = 26
+		targetSdk = 36
+		versionCode = 1
+		versionName = "1.0"
+
+		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+	}
+	buildFeatures {
+		viewBinding = true
+	}
+	buildTypes {
+		release {
+			isMinifyEnabled = false
+			proguardFiles(
+				getDefaultProguardFile("proguard-android-optimize.txt"),
+				"proguard-rules.pro"
+			)
+		}
+	}
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_11
+		targetCompatibility = JavaVersion.VERSION_11
+	}
+	kotlinOptions {
+		jvmTarget = "11"
+	}
+}
+
+dependencies {
+	// AndroidX Core
+	implementation(libs.androidx.core.ktx)
+	implementation(libs.androidx.appcompat)
+	implementation(libs.material)
+	implementation(libs.androidx.activity)
+	implementation(libs.androidx.constraintlayout)
+	implementation(libs.androidx.fragment.ktx)
+
+	// ViewModel, LiveData, Lifecycle
+	implementation(libs.androidx.lifecycle.livedata.ktx)
+	implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
+	// Room DB
+	implementation(libs.androidx.room.runtime)
+	ksp(libs.androidx.room.compiler)
+	implementation(libs.androidx.room.ktx)
+
+	// Navigation Component
+	implementation(libs.androidx.navigation.fragment.ktx)
+	implementation(libs.androidx.navigation.ui.ktx)
+
+	// RecyclerView
+	implementation(libs.androidx.recyclerview)
+
+	// Test dependencies
+	testImplementation(libs.junit)
+	androidTestImplementation(libs.androidx.junit)
+	androidTestImplementation(libs.androidx.espresso.core)
+}
